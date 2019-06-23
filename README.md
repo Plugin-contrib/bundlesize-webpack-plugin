4ebdd3f0de0a4e0ab8f041f1f7d0be6f0006d93c
anikethsaha <anik220798@gmail.com>
2019-06-24 01:41:34 +0530

    v0.2.1

--- README.md
+++ README.md
@@ -1,13 +1,60 @@
-# WIP
-## The Package is still not ready to use
-## Still lot to complte
+> # WIP
+> ## The package is still in development stage
+
 
 
 <p align="center" >
-    <img src="https://imgur.com/SW2cpQr.png" width="200px" />
+    <img src="https://imgur.com/A2YgC4S.png" width="400px" />
 </p>
-<h2 align="center">:zap: webpack-plugin-bundlesize :zap:</h2>
+<h1  align="center"> webpack-plugin-bundlesize 📦 </h1>
 
 <p align="center" >
 Show with plugin or loader is being run currently in CLI
 </p>
+
+
+
+
+## Usage
+1. **Installation**
+```bash
+$ npm i webpack-plugin-bundlesize
+```
+
+2. In your webpack config
+**webpack.config.js** (Your webpack config file)
+```js
+const webpackBundleSize = require("webpack-plugin-bundlesize")
+
+...
+    plugins: [
+        new webpackBundleSize(options),
+        ...
+    ],
+
+...  // rest of your configuration
+
+```
+
+3. `Options` - Optional
+```js
+{
+    sizeLimit : your-size-in-KB // Default is 3
+}
+```
+
+
+
+## TODO
+- [ ] To implement the current in watchRun hook
+
+- [ ] To recommend some optimizations when size exceeding
+
+- [ ] To implement the logo and make the looks better
+
+- [ ] To suggest optmizations
+   - [ ] read the config and check whether the splitchunk is implemented or not
+   - [ ] Minify plugin
+   - [ ] Codespliting suggestion
+
+- [ ] Typescript Migration
--- src/index.js
+++ src/index.js
@@ -1,9 +1,95 @@
-const {help} = require("./check.js")
-const helloworld = () => {
-    console.log("wahr wahahw eolrd")
-    return "whatok"
+/**
+ * TODOs
+ *
+ *  1. To implement the current in watchRun hook
+ *  2. To recommend some optimizations when size exceeding
+ *  3. To implement the logo and make the looks better
+ *  4. To suggest optmizations
+ *    - read the config and check whether the splitchunk is implemented or not
+ *    - Minify plugin
+ *    - Codespliting suggestion
+ *  5. Typescript Migration
+ */
+
+
+
+
+const logo = `
+          _______________
+         /              /
+        /______________/|
+        |             | |
+        |             | |
+        |             | /
+        |_____________|/
+`
+
+
+var fs = require("fs");
+const {
+    info,
+    warn,
+    error
+} = require('prettycli');
+
+// Community Standard Method - https://stackoverflow.com/a/18650828/9339537
+function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return {
+        bundleSize: parseFloat((bytes / Math.pow(k, i)).toFixed(dm)),
+        fullSizeInfo: parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+    }
 }
-module.exports = {
-    helloworld,
-    help
+
+module.exports = class WebpackPluginTesting {
+
+    constructor(pluginOptions) {
+        this.pluginOptions = pluginOptions || {
+            sizeLimit: 10
+        }
+    }
+    apply(compiler) {
+        compiler.hooks.watchRun.tap("BundleSizePlugin", (compilation) => {
+            // TODO
+        })
+        compiler.hooks.done.tap("BundleSizePlugin", (stats) => {
+            const {
+                path,
+                filename
+            } = stats.compilation.options.output;
+            const bundlePath = require("path").resolve(path, filename)
+            const {
+                size
+            } = fs.statSync(bundlePath)
+            const {
+                bundleSize,
+                fullSizeInfo
+            } = formatBytes(size)
+            this.messageLoggin(bundleSize, fullSizeInfo)
+        })
+    }
+    messageLoggin(bundleSize, fullSizeInfo) {
+        const {
+            sizeLimit
+        } = this.pluginOptions
+        if (bundleSize < sizeLimit) {
+            info("Safe:Bundle-Size", fullSizeInfo)
+        } else {
+            if (bundleSize == sizeLimit) {
+                warn("Safe:Bundle-Size", fullSizeInfo)
+            } else {
+                error(fullSizeInfo, {
+                    label: "Unsafe:Bundle-Size",
+                    exit: false
+                })
+            }
+        }
+    }
 }
